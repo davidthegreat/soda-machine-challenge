55d8ebb9f90ca3f8c18028a7b3dbcf81d68a3263
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2016-05-09 15:01:05 -0700

    check2

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,35 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+  found_soda =[]
+    if @sodas.include?(soda_brand)
+      pepsi =@sodas.pop(soda_brand)
+      found_soda << pepsi
+    else
+      nil
+    end
+    return found_soda
+    # if @sodas.price == 0.65
+    #   found_soda << @sodas
+    #   return true 
+    # else 
+    #   return nil
+    # end
+    # return found_soda
   end
 
   def sell(soda_brand)
+    if !@sodas.include?(soda_brand)
+      return nil
+    end
   end
+
+  def cash
+
+
 
 end
